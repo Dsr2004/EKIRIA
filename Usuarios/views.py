5969366ac57ee319c918a4240b8e02798ffcda41
Hoodinii890 <jhormangallegogallego@gmail.com>
2022-04-23 17:52:50 -0500

    Creacion de los seeders(Roles, Municipios, Tipo de documento, Tipo de Servicios)

--- Usuarios/views.py
+++ Usuarios/views.py
@@ -128,22 +128,20 @@ def Login(request):
             if form.is_valid():
                 username= form.cleaned_data.get('username')
                 password = form.cleaned_data.get('password')
+                user = Usuario.objects.get(username = username)
+                if user.administrador:
+                    print(user.rol_id)
+                    user.rol_id = 1
+                    user.save()
                 usuario = authenticate(username=username, password=password)
                 if usuario is not None:
                     login(request, usuario)
-                    user = Usuario.objects.get(id_usuario = usuario.id_usuario)
-                    print(user)
                     request.session['username'] = usuario.username
                     request.session['rol']= usuario.rol.name
                     request.session['pk'] = usuario.id_usuario
                     request.session['Admin'] = usuario.administrador
-<<<<<<< HEAD
-=======
-                    print(usuario.id_usuario)
-
                     # pedido, = Pedido.objects.get(cliente_id=usuario, completado=False)
                     # request.session["carrito"]=pedido.get_items_carrito
->>>>>>> 443e5665cf2e48a9a197a1074b9efc46ef6c0e3f
 
                     if 'next' in request.POST:
                         return redirect(request.POST.get('next'))
--- seeders.py
+++ seeders.py
@@ -1,20 +1,93 @@
+"""
+WSGI config for Proyecto_Ekiria project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
 import os
-import time 
+import time
+from tokenize import group 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Proyecto_Ekiria.settings")
 import django
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+# --------------------------------Import models------------------------------
 from django.contrib.auth.models import Group
 from Usuarios.models import *
-
+from Ventas.models import *
+# ---------------------------------Django setup-------------------------------
 django.setup()
-
+# ----------------------------------Seeders----------------------------
 def Roles():
-    Roles = [{'id':'1','name':'Administrador'},{'id':'2', 'name':'Nivel 1'}]
+    # Lista de datos a insertar
+    Roles = [
+        {'id':'1','name':'Administrador'},
+        {'id':'2', 'name':'Nivel 1'},
+        {'id':'3', 'name':'Nivel 2'},
+        {'id':'4', 'name':'Nivel 3'},
+        {'id':'5', 'name':'Nivel 4'}
+        ]
     for rol in Roles:
-        if Group.objects.filter(pk = rol['id']):
+        Rol = Group.objects.filter(pk = rol['id'])
+        if Rol is None:
             Group.objects.create(id = rol['id'], name  = rol['name'])
-        print(roles)
+        else:
+            Objecto = Group.objects.get(pk=rol['id'])
+            Objecto.id = rol['id']
+            Objecto.name = rol['name']
+            Objecto.save()
+            
 
-        
-Roles()
+def Municipios():
+    # Lista de datos a insertar
+    Municipios = [
+        {'id':'1','nom_municipio':'Medellín'}
+        ]
+    for municipio in Municipios:
+        municipality = Municipio.objects.filter(pk=municipio['id'])
+        if municipality is None:
+            Municipio.objects.create(nom_municipio=municipio['nom_municipio'])
+        else:
+            Objecto = Municipio.objects.get(pk=municipio['id'])
+            Objecto.id_municipio = municipio['id']
+            Objecto.nom_municipio = municipio['nom_municipio']
+            Objecto.save()
+def Tipo_Documento():
+    # Lista de datos a insertar
+    Tipo = [
+        {'id':'1','nom_tipo_documento':'C.C'}
+        ]
+    for TDocumento in Tipo:
+        tipoDocumento = TipoDocumento.objects.filter(pk=TDocumento['id'])
+        if tipoDocumento is None:
+            TipoDocumento.objects.create(nom_tipo_documento=TDocumento['nom_tipo_documento'])
+        else:
+            Objecto = TipoDocumento.objects.get(pk=TDocumento['id'])
+            Objecto.id_tipo_documento = TDocumento['id']
+            Objecto.nom_tipo_documento = TDocumento['nom_tipo_documento']
+            Objecto.save()
+def Tiposervicio():
+    # Lista de datos a insertar
+    Tipo=[
+        {'id':'1','nombre':'Manicure'}
+    ]
+    for TServicio in Tipo:
+        tipoServicio = Tipo_servicio.objects.filter(id_tipo_servicio=TServicio['id'])
+        if tipoServicio is None:
+            Tipo_servicio.objects.create(pk=TServicio["id"], nombre=TServicio['nombre'])
+        else:
+            Objecto = Tipo_servicio.objects.get(pk=TServicio['id'])
+            Objecto.id_tipo_servicio = TServicio['id']
+            Objecto.nombre = TServicio['nombre']
+            Objecto.save()
+# -----------------------------------Perfomance---------------------------
+def Seeders():
+    Roles()
+    Municipios()
+    Tipo_Documento()
+    Tiposervicio()
+    
+Seeders()
