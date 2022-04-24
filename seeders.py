532e152705f1e40476b3982041ea5b278d10f0f3
Hoodinii890 <jhormangallegogallego@gmail.com>
2022-04-23 21:27:47 -0500

    Error?

--- seeders.py
+++ seeders.py
@@ -33,32 +33,41 @@ def Roles():
         {'id':'6', 'name':'Empleado'},
         ]
     for rol in Roles:
-        Rol = Group.objects.filter(pk = rol['id'])
-        for queryset in Rol:
-            if queryset is not None:
-                Objecto = Group.objects.get(pk=rol['id'])
-                Objecto.id = rol['id']
-                Objecto.name = rol['name']
-                Objecto.save()
-            else:
+        try:
+            Objecto = Group.objects.get(pk=rol['id'])
+            Objecto.id = rol['id']
+            Objecto.name = rol['name']
+            Objecto.save()
+        except:
+            try:
+                Objecto=Group.objects.get(id = rol['id'])
+                Objecto.delete()
+                Group.objects.create(id = rol['id'], name  = rol['name'])
+            except:
                 Group.objects.create(id = rol['id'], name  = rol['name'])
             
 
 def Municipios():
     # Lista de datos a insertar
     Municipios = [
-        {'id':'1','nom_municipio':'Medellín'}
+        {'id':'1','nom_municipio':'Medellín'},
+        {'id':'2','nom_municipio':'Bello'},
         ]
     for municipio in Municipios:
-        municipality = Municipio.objects.filter(pk=municipio['id'])
-        for queryset in municipality:
-            if queryset is None:
-                Municipio.objects.create(nom_municipio=municipio['nom_municipio'])
-            else:
-                Objecto = Municipio.objects.get(pk=municipio['id'])
-                Objecto.id_municipio = municipio['id']
-                Objecto.nom_municipio = municipio['nom_municipio']
-                Objecto.save()
+        try:
+            Objecto = Municipio.objects.get(pk=municipio['id'])
+            Objecto.id_municipio = municipio['id']
+            Objecto.nom_municipio = municipio['nom_municipio']
+            Objecto.save()
+        except:
+            try:
+                Objecto = Municipio.objects.get(nom_municipio=municipio['nom_municipio'])
+                Objecto.delete()
+                Municipio.objects.create(pk=municipio['id'],nom_municipio=municipio['nom_municipio'])
+            except:
+                Municipio.objects.create(pk=municipio['id'],nom_municipio=municipio['nom_municipio'])
+                
+
                 
 def Tipo_Documento():
     # Lista de datos a insertar
@@ -66,31 +75,37 @@ def Tipo_Documento():
         {'id':'1','nom_tipo_documento':'C.C'}
         ]
     for TDocumento in Tipo:
-        tipoDocumento = TipoDocumento.objects.filter(pk=TDocumento['id'])
-        for queryset in tipoDocumento:
-            if queryset is None:
-                TipoDocumento.objects.create(nom_tipo_documento=TDocumento['nom_tipo_documento'])
-            else:
-                Objecto = TipoDocumento.objects.get(pk=TDocumento['id'])
-                Objecto.id_tipo_documento = TDocumento['id']
-                Objecto.nom_tipo_documento = TDocumento['nom_tipo_documento']
-                Objecto.save()
-                
+        try:
+            Objecto = TipoDocumento.objects.get(pk=TDocumento['id'])
+            Objecto.id_tipo_documento = TDocumento['id']
+            Objecto.nom_tipo_documento = TDocumento['nom_tipo_documento']
+            Objecto.save()
+        except:
+            try:
+                Objecto=TipoDocumento.objects.get(nom_tipo_documento=TDocumento['nom_tipo_documento'])
+                Objecto.delete()
+                TipoDocumento.objects.create(pk=TDocumento['id'],nom_tipo_documento=TDocumento['nom_tipo_documento'])
+            except:
+                TipoDocumento.objects.create(pk=TDocumento['id'],nom_tipo_documento=TDocumento['nom_tipo_documento'])
+            
 def Tiposervicio():
     # Lista de datos a insertar
     Tipo=[
         {'id':'1','nombre':'Manicure'}
     ]
     for TServicio in Tipo:
-        tipoServicio = Tipo_servicio.objects.filter(id_tipo_servicio=TServicio['id'])
-        for queryset in tipoServicio:
-            if queryset is None:
+        try:
+            Objecto = Tipo_servicio.objects.get(pk=TServicio['id'])
+            Objecto.id_tipo_servicio = TServicio['id']
+            Objecto.nombre = TServicio['nombre']
+            Objecto.save()
+        except:
+            try:
+                Objecto=Tipo_servicio.objects.get(pk=TServicio["id"])
+                Objecto.delete()
                 Tipo_servicio.objects.create(pk=TServicio["id"], nombre=TServicio['nombre'])
-            else:
-                Objecto = Tipo_servicio.objects.get(pk=TServicio['id'])
-                Objecto.id_tipo_servicio = TServicio['id']
-                Objecto.nombre = TServicio['nombre']
-                Objecto.save()
+            except:
+                Tipo_servicio.objects.create(pk=TServicio["id"], nombre=TServicio['nombre'])
             
 def Rol_Permisos():
     roles = Group.objects.all()
