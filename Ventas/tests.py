8f70ccc06d2ef096dec7198a5d0df42db0bcc90a
Dsr2004 <davitdy2015@gmail.com>
2022-05-19 21:08:36 -0500

    puse la hora que faltaba cuando se agendaba una cita | encontre un error en el calendario de terminar pedido

--- Ventas/tests.py
+++ Ventas/tests.py
@@ -1,9 +1,11 @@
-import smtplib
-from Proyecto_Ekiria.wsgi import *
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from django.template.loader import render_to_string
-from Proyecto_Ekiria import settings
+# import smtplib
+# from Proyecto_Ekiria.wsgi import *
+# from email.mime.text import MIMEText
+# from email.mime.multipart import MIMEMultipart
+# from django.template.loader import render_to_string
+# from Proyecto_Ekiria import settings
+
+import datetime as dt
 
 
 def send_email():
@@ -31,6 +33,58 @@ def send_email():
     except Exception as e:
         print(e)
 
-send_email()
 
 
+
+
+
+def restar():
+    #lista donde estan todas las horas disponibles
+    horas = hours = [(dt.time(i).strftime("%H:%M")) for i in range(24)]
+
+    #horas que hay que quitar de la lista principal
+    horasNoDisponibles = {'cita1': {'horaInicio': '04:00', 'horaFin': '06:00'}, 'cita2': {'horaInicio': '07:00', 'horaFin': '09:00'}}
+
+    #funcion que resta
+    # res=[]
+    # for i in horasNoDisponibles:
+    #     for x in horas:
+    #         if  x not in res:
+    #             if (x < horasNoDisponibles[i]["horaInicio"] or x >horasNoDisponibles[i]["horaFin"]):
+    #                 res.append(x)
+   
+    
+    res = [x 
+        for x in horas
+            for i in horasNoDisponibles if i not in res  
+            if (x < horasNoDisponibles[i]["horaInicio"] or x >horasNoDisponibles[i]["horaFin"])
+            ]
+        
+
+    print(res)
+
+def prueba():
+    horas = [(dt.time(i).strftime("%H:%M")) for i in range(24)]
+
+    horasNoDisponibles = {'cita1': {'horaInicio': '04:00', 'horaFin': '06:00'}, 'cita2': {'horaInicio': '07:00', 'horaFin': '09:00'}}
+
+    for i in horasNoDisponibles:
+        res = [x for x in horas if (x < horasNoDisponibles[i]["horaInicio"] or x > horasNoDisponibles[i]["horaFin"])]
+
+    print(res)
+
+prueba()
+
+# 12
+
+
+# hours = [(dt.time(i).strftime("%H:%M")) for i in range(24)]
+
+# for i in hours:
+#     print(i, type(i))
+
+
+
+
+# como convertir el numero a hora en python
+# hours = [(dt.time(hour=i)) for i in range(24)]
