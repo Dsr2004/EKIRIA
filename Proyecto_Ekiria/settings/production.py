614f83dd24d1690159fd1f1a64aac74ad0c77b6b
Dsr2004 <davitdy2015@gmail.com>
2022-06-25 16:03:53 -0500

    change to deploy mod

--- Proyecto_Ekiria/settings/production.py
+++ Proyecto_Ekiria/settings/production.py
@@ -11,10 +11,6 @@ DATABASES = {
         'PASSWORD': '690bc2558a6cf8313324ea8ffe7483a74e9f3d452b9bbd9bcd2e0351e3265055',
         'HOST': 'ec2-34-200-35-222.compute-1.amazonaws.com',
         'PORT': '5432',
-        'OPTIONS': {
-            'init_command': 'SET default_storage_engine=INNODB',
-            'charset': 'utf8mb4',
-        }
     }
 }
 
