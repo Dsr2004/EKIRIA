adae08f3e2718863b5d9ef17d32e329956c9481c
Dsr2004 <davitdy2015@gmail.com>
2022-06-28 08:00:47 -0500

    whitenoise

--- Proyecto_Ekiria/wsgi.py
+++ Proyecto_Ekiria/wsgi.py
@@ -16,6 +16,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Proyecto_Ekiria.settings.produc
 
 # application = get_wsgi_application()
 
-from dj_static import Cling
-application = Cling(get_wsgi_application())
+# from dj_static import Cling
+# application = Cling(get_wsgi_application())
 
+from whitenoise.django import DjangoWhiteNoise
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
