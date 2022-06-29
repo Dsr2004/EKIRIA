269be2c17e5434787088a12e52d6c52cb7bde709
Dsr2004 <davitdy2015@gmail.com>
2022-06-28 20:32:43 -0500

    admin ventas

--- Ventas/admin.py
+++ Ventas/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Cita
+from .models import Cita, Calendario, Pedido, PedidoItem
 
 admin.site.register(Cita)
- 
+admin.site.register(Calendario)
+admin.site.register(Pedido)
+admin.site.register(PedidoItem)
 
 # Register your models here.
